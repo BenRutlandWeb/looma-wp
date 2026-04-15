c8af6cc760e19221f26bbd700f41df2158520baa
Ben Rutland <benrutland@hotmail.com>
2026-04-15 22:50:53 +0100

    updated wp config

--- public/wp-config.php
+++ public/wp-config.php
@@ -8,46 +8,28 @@ $env = $dotenv->safeLoad();
 
 $dotenv->required(['APP_URL', 'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
 
-$disallowed_vars = [
-    'DISALLOW_FILE_EDIT',
-    'DISALLOW_FILE_MODS',
-    'WP_AUTO_UPDATE_CORE',
-    'AUTOMATIC_UPDATER_DISABLED',
+$app_url = rtrim($env['APP_URL'], '/');
+
+$table_prefix = $env['DB_PREFIX'] ?? 'wp_';
+
+$required_vars = [
+    'WP_DEBUG'                   => $env['APP_DEBUG'] ?? false,
+    'WP_HOME'                    => $app_url,
+    'WP_SITEURL'                 => $app_url . '/admin',
+    'WP_CONTENT_URL'             => $app_url . '/app',
+    'WP_CONTENT_DIR'             => __DIR__ . '/app',
+    'WP_ENVIRONMENT_TYPE'        => $env['APP_ENV'] ?? 'production',
+    'DISALLOW_FILE_EDIT'         => true,
+    'DISALLOW_FILE_MODS'         => true,
+    'WP_AUTO_UPDATE_CORE'        => false,
+    'AUTOMATIC_UPDATER_DISABLED' => true,
+    'ABSPATH'                    => __DIR__ . '/',
 ];
 
-foreach ($disallowed_vars as $key) {
-    unset($env[$key]);
-}
-
-foreach ($env as $key => $value) {
+foreach (array_merge($required_vars, $env) as $key => $value) {
     if (!defined($key)) {
         define($key, $value);
     }
 }
 
-$table_prefix = $env['DB_PREFIX'] ?? 'wp_';
-
-if (! defined('WP_DEBUG')) {
-    define('WP_DEBUG', $env['APP_DEBUG'] ?? false);
-}
-
-define('WP_HOME', rtrim($env['APP_URL'], '/'));
-define('WP_SITEURL', WP_HOME . '/admin');
-define('WP_CONTENT_DIR', __DIR__ . '/app');
-define('WP_CONTENT_URL', WP_HOME . '/app');
-
-/* */
-define('WP_ENVIRONMENT_TYPE', $env['APP_ENV'] ?? 'production');
-define('DISALLOW_FILE_EDIT', true);
-define('DISALLOW_FILE_MODS', true);
-define('WP_AUTO_UPDATE_CORE', false);
-define('AUTOMATIC_UPDATER_DISABLED', true);
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
-if (! defined('ABSPATH')) {
-    define('ABSPATH', __DIR__ . '/');
-}
-
-/** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
